f00078b4ce413bc0dcc259e7615931a17ae80e80
LucaPalminteri <lucapalminteri02@gmail.com>
2022-11-04 07:48:18 -0300

    fix hours bug

--- pages/insert.jsx
+++ pages/insert.jsx
@@ -11,15 +11,16 @@ function insert() {
         let date = new Date()
 
         let type = ""
-        let hours = date.getHours()
+        let hours = date.getHours() - 3
 
+        if (hours < 0) (hours * (-1)) + 21
         if(hours >= 0 && hours < 10) type = "Breakfast"
-        if(hours >= 10 && hours < 12) type = "Breakfast Snack"
-        if(hours >= 12 && hours < 14) type = "Lunch"
-        if(hours >= 14 && hours < 16) type = "Afterlunch Snack"
-        if(hours >= 16 && hours < 19) type = "Afternoon Snack"
-        if(hours >= 19 && hours < 21) type = "Night Snack"
-        if(hours >= 21 && hours < 24) type = "Dinner"
+        else if(hours >= 10 && hours < 12) type = "Breakfast Snack"
+        else if(hours >= 12 && hours < 14) type = "Lunch"
+        else if(hours >= 14 && hours < 16) type = "Afterlunch Snack"
+        else if(hours >= 16 && hours < 19) type = "Afternoon Snack"
+        else if(hours >= 19 && hours < 21) type = "Night Snack"
+        else if(hours >= 21 && hours < 24) type = "Dinner"
 
         try {
             const { data, error} = await supabase.from('meals').insert({
