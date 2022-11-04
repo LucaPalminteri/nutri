4e1c57c82680e8489cd48727fd6155cb9324144f
LucaPalminteri <lucapalminteri02@gmail.com>
2022-11-03 21:00:37 -0300

    update index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,18 @@ import type { AppProps } from 'next/app'
 import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const style = {backgroundColor: '#DEDEDE', padding: '4px 8px', borderRadius: '8px'}
+  let isHome = true
+
+if(window.location.pathname == '/') isHome = true
+else isHome = false
+
   return (
     <>
     <nav>
-      <Link href='/'>Home</Link>
-      <Link href='/insert'>Insert</Link>
+      <Link href='/' style={isHome ? style : {padding: '4px'}}>Home</Link>
+      <Link href='/insert' style={!isHome ? style : {padding: '4px'}}>Insert</Link>
     </nav>
     <Component {...pageProps} />
     </>
