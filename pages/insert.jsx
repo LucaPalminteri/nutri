827ea67d3a69d185e9f9efad98534cd57167dbe0
LucaPalminteri <lucapalminteri02@gmail.com>
2022-11-13 08:53:54 -0300

    change logic & add database info nutrition

--- pages/insert.jsx
+++ pages/insert.jsx
@@ -1,43 +1,72 @@
 import React, { useRef } from 'react'
+import { useEffect, useState } from 'react';
 import insertDB from '../functions/insertDB';
-
+import { supabase } from '../supabaseClient'
 function insert() {
 
+    useEffect(() => {
+        getFood()
+    },[])
+
+    const [foods, setFoods] = useState([])
+    const [selectedFoods, setsSelectedFoods] = useState([])
+    
     const description = useRef();
+
+    const getFood = async () => {
+        try {
+            const {data,error} = await supabase.from("foods").select("*")
+            setFoods(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    console.log(foods);
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        return ;
         await insertDB(description.current.value)
         description.current.value = ""
     }
 
-    const handleCustomInsert = async (n) => {
-        switch(n){
-            case 1: await insertDB("1 Cappuccino Cup")
-                break;
-            case 2: await insertDB("1 Glass of Water")
-                break;
-            case 3: await insertDB("1 Handful of Nuts")
-                break;
-            case 4: await insertDB("4 toast with Penaut Butter")
-                break;
-        }
-
-        alert('OK')
+    const optionFoods = ()=> {
+        return foods.sort((a, b) => {
+            let fa = a.name.toLowerCase(),
+                fb = b.name.toLowerCase();
+        
+            if (fa < fb) {
+                return -1;
+            }
+            if (fa > fb) {
+                return 1;
+            }
+            return 0;
+        })
+        .map(food => {
+            return (
+                <option className='option-food' key={food.id} value={food.name}>{food.name}</option>
+            )
+        })
     }
 
+    const selectFoods = () => {
+        setsSelectedFoods(prev => prev.concat(<select className='select-food' key={Math.random()} ref={description}>
+            <option value="default" selected>~~ food ~~</option>
+            {optionFoods()}
+        </select>)) 
+    }
+
+    console.log(selectedFoods);
+
   return (
-    <div>
+    <div className='insert'>
         <form onSubmit={(e) => handleSubmit(e)}>
-            <textarea ref={description}></textarea>
+            {selectedFoods}
+            <button onClick={()=> selectFoods()}>+</button>
             <button type='submit'>Submit</button>
         </form>
-        <div className='btn-container'>
-            <button className='shortcut-btn' onClick={() => handleCustomInsert(1)}>Cappuccino</button>
-            <button className='shortcut-btn' onClick={() => handleCustomInsert(2)}>Glass of Water</button>
-            <button className='shortcut-btn' onClick={() => handleCustomInsert(3)}>Handful of Nuts</button>
-            <button className='shortcut-btn' onClick={() => handleCustomInsert(4)}>4 toast with Penaut Butter</button>
-        </div>
     </div>
   )
 }
