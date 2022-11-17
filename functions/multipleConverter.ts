3e058de5805bf4fcc6d33312717a960662acbde9
LucaPalminteri <lucapalminteri02@gmail.com>
2022-11-17 07:54:20 -0300

    feat: upgrade convert unit funcion

--- functions/multipleConverter.ts
+++ functions/multipleConverter.ts
@@ -1,8 +1,10 @@
-export function multipleConverter(n: number): number {
+export function multipleConverter(n: number,unit:string): string {
 
-    if (n >= 1 && n < 1000) return n;
-    if (n >= 0.001 && n < 1) return n * 1_000;
-    if (n >= 0.000001 && n < 0.001) return n * 1_000_000;
+    let out:string = '';
 
-    else return n;
+    if (n >= 1 && n < 1000)  return `${n.toString()} ${unit}`;
+    if (n >= 0.001 && n < 1)  return `${(n * 1_000).toString()} m${unit}`;
+    if (n >= 0.000001 && n < 0.001)  return `${(n * 1_000_000).toString()} µ${unit}`;
+
+    return `${n.toString()} ${unit}`;
 }
